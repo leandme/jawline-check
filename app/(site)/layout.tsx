3146b97a52e6338c447883b68ef63cec21b1730c
Matt Phelps <matt@leandme.com>
2026-05-10 09:50:20 +0200

    Add Google Analytics gtag to site layout

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+import Script from "next/script";
 import AmplitudeInitializer from "../components/AmplitudeInitializer";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
@@ -11,7 +12,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-base-100 text-base-content">
-      <AmplitudeInitializer />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-6LLRSM3Y4E"
+          strategy="afterInteractive"
+        />
+        <Script id="ga-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-6LLRSM3Y4E');
+          `}
+        </Script>
+        <AmplitudeInitializer />
         <Navbar />
         <main className="container mx-auto px-4 lg:px-8 py-8 min-h-screen">
           {children}
